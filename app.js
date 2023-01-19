b7b32735d4aabcf8a0c8fbe0c515ee33517538aa
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-01-19 21:03:20 +0300

    Prepare layout for user
    Layout for users is created into layouts folder, and partials are implemented in it.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express'); 
-const app = express();  
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('HELLOOO');
 });
 
 const PORT = process.env.PORT || 80;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
