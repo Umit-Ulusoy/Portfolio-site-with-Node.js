d6ba38c78b4753b49aac28f1d1a7c862372d0ab4
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-01-22 13:01:15 +0300

    Prepare about page
    Prepared "about.ejs" and "getAboutPage" module in "pageController.js" files, and "getAboutPage" module is connected to its router.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,6 +1,22 @@
 const Portfolio = require('../models/portfolio');
 const Service = require('../models/service');
 
+exports.getAboutPage = async (req, res) => {
+    try {
+        res.status(200).render('layouts/user', {
+            fileName: 'about'
+        });
+    } catch (error) {
+        res.status(400).send('Something went wrong! Please try again later.');
+    }
+}
+
+exports.getContactPage = async (req, res) => {
+    res.status(200).render('layouts/user', {
+        fileName: 'contact'
+    });
+}
+
 exports.getHomePage = async (req, res) => {
         res.status(200).render('layouts/user', {
             fileName: 'home'
@@ -9,15 +25,17 @@ exports.getHomePage = async (req, res) => {
 }
 
 exports.getPortfolioPage = async (req, res) => {
+try {
     const portfolios = await Portfolio.find();
         res.status(200).render('layouts/user', {
             fileName: 'portfolio',
             portfolios
-        });
-
+        });    
+} catch (error) {
+    res.status(400).send('Something went wrong! Please try again later.');
 }
 
-exports.getPortfolioPage 
+}
 
 exports.getServicePage = async (req, res) => {
 try {
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -3,6 +3,8 @@ const pageController = require('../controllers/pageController');
 
 const router = express.Router();
 
+router.route('/about').get(pageController.getAboutPage);
+router.route('/contact').get(pageController.getContactPage);
 router.route('^(\/|\/home)$').get(pageController.getHomePage);
 router.route('/portfolio').get(pageController.getPortfolioPage);
 router.route('/services').get(pageController.getServicePage);
