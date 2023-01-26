8ee9394f6e693efb15e41934e20d80c33349c0bc
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-01-27 02:04:01 +0300

    Create functions for add and delete actions of project section
    "addProject" and "deleteProject" functions are created in "portfolioController.js" file in "controllers/auth/" directory to add and delete projects, and connected to its router.

--- controllers/auth/portfolioController.js
+++ controllers/auth/portfolioController.js
@@ -1,10 +1,30 @@
 const PortfolioPage = require('../../models/portfolio');
 const Project = require('../../models/project');
 
+exports.addProject = async (req, res) => {
+    try {
+        await Project.create(req.body);
+
+        res.status(201).redirect('/admin/portfolio');
+    } catch (error) {
+        res.status(400).send('Something went wrong! Please try again later.');
+    }
+}
+
+exports.deleteProject = async (req, res) => {
+    try {
+        await Project.deleteOne({ slug: req.params.slug });
+
+        res.status(200).redirect('/admin/portfolio');
+    } catch (error) {
+        res.status(400).send('Something went wrong! Please try again later.');
+    }
+}
+
 exports.getPortfolioPage = async (req, res) => {
     try {
         const portfolioPage = await PortfolioPage.findOne();
-        const projects = await Project.find();
+        const projects = await Project.find().sort('-createdAt');
 
         res.status(200).render('layouts/admin', {
             fileName: 'portfolio',
@@ -34,7 +54,7 @@ exports.updatePortfolioPage = async (req, res) => {
 exports.updateProject = async (req, res) => {
     try {
         const { name, description, link } = req.body;
-        const project = await Project.findOne({ _id: req.params.projectId });
+        const project = await Project.findOne({ slug: req.params.slug });
 
         project.name = req.body.name;
         project.description = req.body.description;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -8,7 +8,9 @@ router.route('/login').get(authController.getLoginPage);
 router.route('/home').get(homeController.getHomePage);
 router.route('/home').put(homeController.updateHomePage);
 router.route('/portfolio').get(portfolioController.getPortfolioPage);
+router.route('/portfolio').post(portfolioController.addProject);
+router.route('/portfolio/:slug').delete(portfolioController.deleteProject);
 router.route('/portfolio').put(portfolioController.updatePortfolioPage);
-router.route('/portfolio/:projectId').put(portfolioController.updateProject);
+router.route('/portfolio/:slug').put(portfolioController.updateProject);
 
 module.exports = router;
